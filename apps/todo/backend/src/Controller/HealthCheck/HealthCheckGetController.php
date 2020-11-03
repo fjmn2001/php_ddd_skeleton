d6bea6d24d491aec59354be5265ed317ce9e763e
Francisco Marcano <fjmn2001@gmail.com>
2020-11-03 19:00:03 -0400

    feat(): installing acceptance test environment v8

--- apps/todo/backend/src/Controller/HealthCheck/HealthCheckGetController.php
+++ apps/todo/backend/src/Controller/HealthCheck/HealthCheckGetController.php
@@ -4,15 +4,23 @@ declare(strict_types=1);
 
 namespace Medine\Apps\Todo\Backend\Controller\HealthCheck;
 
+use Medine\Shared\Domain\RandomNumberGenerator;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 final class HealthCheckGetController
 {
+    private $generator;
+
+    public function __construct(RandomNumberGenerator $generator)
+    {
+        $this->generator = $generator;
+    }
+
     public function __invoke(): JsonResponse
     {
         return new JsonResponse([
             'todo-backend' => 'ok',
-            'rand' => rand()
+            'rand' => $this->generator->generate()
         ]);
     }
 }
