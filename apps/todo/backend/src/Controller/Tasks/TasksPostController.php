085030609e5c0ace9e86eafe20f3ced4f8851fa2
Francisco Marcano <fjmn2001@gmail.com>
2020-12-08 14:01:06 -0400

    feat(): tasks crud v1. Ref SN

---
+++ apps/todo/backend/src/Controller/Tasks/TasksPostController.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Medine\Apps\Todo\Backend\Controller\Tasks;
+
+use Medine\Shared\Infrastructure\Symfony\ApiController;
+use Medine\Todo\Tasks\Application\Create\CreateTaskCommand;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+final class TasksPostController extends ApiController
+{
+    public function __invoke(Request $request): Response
+    {
+        $this->dispatch(
+            new CreateTaskCommand(
+                $request->get('id'),
+                $request->get('name')
+            )
+        );
+
+        return new Response('', Response::HTTP_CREATED);
+    }
+
+    protected function exceptions(): array
+    {
+        return [];
+    }
+}
